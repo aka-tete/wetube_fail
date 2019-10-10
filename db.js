a93361342fcc11f5cab84148f2c03697e6e302e6
aka-tete <54326305+aka-tete@users.noreply.github.com>
2019-10-11 07:35:17 +0900

    Home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 6717,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 67176717,
+            name: "Tete",
+            email: "tete6717@gmail.com"
+        }
+    },
+    {
+        id: 6845,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 67176717,
+            name: "Tete",
+            email: "tete6717@gmail.com"
+        }
+    },
+    {
+        id: 874513,
+        title: 'Video Sexy',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 67176717,
+            name: "Tete",
+            email: "tete6717@gmail.com"
+        }
+    },
+    {
+        id: 874421,
+        title: 'Video Cute',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 67176717,
+            name: "Tete",
+            email: "tete6717@gmail.com"
+        }
+    }
+];

e3b434566e2dd7797def11f284fc30efe3c1ba2f
aka-tete <54326305+aka-tete@users.noreply.github.com>
2019-10-15 01:45:35 +0900

    mongo model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,35 @@
-export const join = (req, res) =>
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
-export const login = (req, res) =>
+
+};
+
+export const postJoin = (req, res) => {
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if (password !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle: "Join" });
+    } else {
+        // To Do: Register User
+        // To DO: Log user in
+        res.redirect(routes.home)
+    }
+};
+
+export const getLogin = (req, res) =>
     res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) =>
-    res.render("logout", { pageTitle: "Logout" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+};
+
 export const users = (req, res) =>
     res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
--- db.js
+++ db.js
@@ -1,50 +1,20 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
     {
-        id: 6717,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 67176717,
-            name: "Tete",
-            email: "tete6717@gmail.com"
-        }
-    },
-    {
-        id: 6845,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 67176717,
-            name: "Tete",
-            email: "tete6717@gmail.com"
-        }
-    },
-    {
-        id: 874513,
-        title: 'Video Sexy',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 67176717,
-            name: "Tete",
-            email: "tete6717@gmail.com"
-        }
-    },
-    {
-        id: 874421,
-        title: 'Video Cute',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 67176717,
-            name: "Tete",
-            email: "tete6717@gmail.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);

1d36512c3cbcf35eacb791b1ec816bd5132c58ca
aka-tete <54326305+aka-tete@users.noreply.github.com>
2019-11-17 02:09:21 +0900

    20191117-editProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,19 +40,20 @@ export const postLogin = passport.authenticate("local", {
 export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
-    const { _json: { id, avatar_url, name, email } } = profile;
+    console.log(_, __, profile, cb);
+    const { _json: { id, avatar_url: avatarUrl, name, email } } = profile;
     try {
         const user = await User.findOne({ email });
         if (user) {
-            user.gihubId = id;
+            user.githubId = id;
             user.save();
             return cb(null, user);
         }
         const newUser = await User.create({
             email,
             name,
-            gihubId: id,
-            avatar_url: avatar_url
+            githubId: id,
+            avatarUrl
         });
         return cb(null, newUser);
     } catch (error) {
@@ -64,16 +65,72 @@ export const postGithubLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = async (_, __, profile, cb) => {
+    const { _json: { id, name, email } } = profile;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            user.facebookId = id;
+            user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+            user.save();
+            return cb(null, user);
+        }
+        const newUser = await User.create({
+            email,
+            name,
+            facebookId: id,
+            avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+        });
+        return cb(null, newUser);
+    } catch (error) {
+        return cb(error);
+    }
+};
+
+export const postFacebookLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => {
     req.logout();
     res.redirect(routes.home);
 };
 
-export const users = (req, res) =>
-    res.render("users", { pageTitle: "Users" });
-export const userDetail = (req, res) =>
-    res.render("userDetail", { pageTitle: "User Detail" });
-export const editProfile = (req, res) =>
+export const getMe = (req, res) => {
+    res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+};
+
+export const userDetail = async (req, res) => {
+    const { params: { id } } = req;
+    try {
+        const user = await User.findById(id);
+        res.render("userDetail", { pageTitle: "User Detail", user });
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
+
+export const getEditProfile = (req, res) =>
     res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+    const {
+        body: { name, email },
+        file
+    } = req;
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            name,
+            email,
+            avatarUrl: file ? file.path : req.user.avatarUrl
+        });
+        res.redirect(routes.me);
+    } catch (error) {
+        res.render("editProfile", { pageTitle: "Edit Profile" });
+    }
+};
+
 export const changePassword = (req, res) =>
     res.render("changePassword", { pageTitle: "Change Password" });
